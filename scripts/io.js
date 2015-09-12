e73b429856ea00dec89a7b503e8f7e2ffe1b7bca
Sam Beaven <sambeaven@gmail.com>
2015-09-12 19:27:41 +0100

    Move saving and loading into a separate file (pt 2) (I suck at git)

---
+++ scripts/io.js
@@ -0,0 +1,23 @@
+define(["../bower_components/store/store.min"], function(store){
+	function save(map, agents){
+		var saveObject = {};
+        saveObject.map = map.toJson(agents.player);
+        saveObject.player = agents.toJson();
+
+        store.set('SamsRogueGame', saveObject);
+	}
+
+	function load(player){
+		var saveObject = store.get('SamsRogueGame');
+        if (saveObject){
+          player.x = saveObject.player.x;
+          player.y = saveObject.player.y;
+        }
+	}
+
+	return {
+		save: save,
+		load: load
+	}
+
+})
